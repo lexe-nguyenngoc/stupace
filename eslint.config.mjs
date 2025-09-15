609b4d3c7802c1aaad79716a25f4261cae52209c
Lexe Nguyen <ngocle.ng1001@gmail.com>
2025-09-15 14:56:02 +0700

    chore: update prettier and eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,7 +14,7 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  { ignores: ["node_modules"] },
+  { ignores: ["node_modules", "components/ui/"] },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   js.configs.recommended,
   ...tseslint.configs.recommended,
